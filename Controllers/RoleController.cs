00a412671b7f9906b55b3cb8fdfeb54644fdc1f4
shehroz665 <shehrozmalik665@gmail.com>
2026-05-29 05:13:18 +0500

    Added Empty Role Controller

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+    }
+}
